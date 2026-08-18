d221874adf0a2e4921c55b4d985023e7905b41e2
ogzA <akankan.oguz@gmail.com>
2026-08-18 14:07:23 +0200

    test: get pokemon data

---
+++ script.js
@@ -0,0 +1,20 @@
+const allPokemons = [];
+const MAIN_PKM_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+function init() {
+	getData();
+}
+
+function render() {}
+
+async function getData() {
+	const response = await fetch(MAIN_PKM_URL);
+
+	if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
+	}
+
+	const result = await response.json();
+
+	console.log(result.results);
+}
